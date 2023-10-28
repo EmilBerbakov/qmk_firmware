3bad087225932c4053e275be1d86a8a8285fde15
Emil Berbakov <emberbakov@gmail.com>
2023-10-28 02:04:54 -0500

    messing with colors again for the last time without error

--- keyboards/keychron/v1/ansi_encoder/keymaps/EmilBerbakov/keymap.c
+++ keyboards/keychron/v1/ansi_encoder/keymaps/EmilBerbakov/keymap.c
@@ -98,7 +98,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 */
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-	for (uint8_t i = led_min; i < led_max; i++ {
+	for (uint8_t i = led_min; i < led_max; i++) {
 		if (host_keyboard_led_state().caps_lock) {
 			rgb_matrix_set_color(i, RGB_WHITE);
 		}
