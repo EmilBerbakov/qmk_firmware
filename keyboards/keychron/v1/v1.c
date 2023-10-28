c1463d7e8e41650d20a77481bc3abecbd04ec54f
Emil Berbakov <emberbakov@gmail.com>
2023-10-28 02:10:17 -0500

    messing with colors adding back color control

--- keyboards/keychron/v1/v1.c
+++ keyboards/keychron/v1/v1.c
@@ -39,6 +39,7 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
 
 #endif
 
+
 #if defined(RGB_MATRIX_ENABLE) && defined(CAPS_LOCK_LED_INDEX)
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
@@ -71,6 +72,7 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
     if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) { return false; }
     // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
 
+    /*
     if (host_keyboard_led_state().caps_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 255, 255, 255);
     } else {
@@ -78,7 +80,9 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
            RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 0, 0, 0);
         }
     }
+    */
     return true;
 }
 
 #endif // CAPS_LOCK_LED_INDEX
+
